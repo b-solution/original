1a67055ad3c152ad3e9cffecf2b1e3a2837ad10f
bilel-kedidi <bilel.kedidi@gmail.com>
2014-08-20 20:34:25 +0100

    theme site original

---
+++ app/controllers/masonry_controller.rb
@@ -0,0 +1,4 @@
+class MasonryController < ApplicationController
+  def index
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Original::Application.config.secret_token = '6957e7e48d63d1b99e4b8aac90fa857f3d6787c3cdd593083cad6f7848f841702335c01dccadc9967e0b1ed24974783b814325b6c38e1b45c33f522a92574456'
